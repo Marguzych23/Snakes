0638f4276e21dd6757a0aa78e3d4b80258c7b5c3
Marguzych <zaynullin.emil@gmail.com>
2018-04-17 21:08:33 +0300

    fix

--- models/Snake.php
+++ models/Snake.php
@@ -8,5 +8,89 @@
 
 class Snake
 {
+    private $head;
+    private $body;
+    private $tail;
+    private $isBite;
+
+    /**
+     * Snake constructor.
+     * @param $head
+     * @param $body
+     * @param $tail
+     * @param $isBite
+     */
+    public function __construct($head, $body, $tail, $isBite)
+    {
+        $this->head = $head;
+        $this->body = $body;
+        $this->tail = $tail;
+        $this->isBite = $isBite;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHead()
+    {
+        return $this->head;
+    }
+
+    /**
+     * @param mixed $head
+     */
+    public function setHead($head)
+    {
+        $this->head = $head;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTail()
+    {
+        return $this->tail;
+    }
+
+    /**
+     * @param mixed $tail
+     */
+    public function setTail($tail)
+    {
+        $this->tail = $tail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getisBite()
+    {
+        return $this->isBite;
+    }
+
+    /**
+     * @param mixed $isBite
+     */
+    public function setIsBite($isBite)
+    {
+        $this->isBite = $isBite;
+    }
+
 
 }

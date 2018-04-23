c180960a5eec5512ed5a2b3f9fef0c3f47d365ce
Marguzych <zaynullin.emil@gmail.com>
2018-04-23 15:45:38 +0300

    fix

---
+++ services/GameService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marguzych
+ * Date: 23.04.2018
+ * Time: 15:39
+ */
+
+namespace services;
+
+
+interface GameService
+{
+
+    public function getStepForSmartSnake();
+}
